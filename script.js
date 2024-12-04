f2694d493412d3385c3303eafd078be05cd5bbad
JulieKodehode <julie.kodehode@gmail.com>
2024-12-04 14:31:06 +0100

    Added some pointers for task

--- script.js
+++ script.js
@@ -17,19 +17,25 @@ addButton.addEventListener("click", function (){
     todoItem.textContent = inputFieldValue;
     todoList.appendChild(todoItem);
 
+    // Explain what happens on the next lines
     const finishedButton = document.createElement("button");
     finishedButton.textContent = "Ferdig";
     todoItem.appendChild(finishedButton);
     finishedButton.addEventListener("click", function () {
         console.log("Ferdig knappen er trykket!");
+
+        // Use an if/else statement here: to check if the element have this styling or not to toggle it on and off.
         todoItem.style.textDecoration = "line-through";
     });
 
+    // Explain what happens on the next lines
     const removeButton = document.createElement("button");
     removeButton.textContent = "Slett";
     todoItem.appendChild(removeButton);
     removeButton.addEventListener("click", function () {
         console.log("Slett knappen er trykket!");
+
+        // Use an if/else statement here: to check if the element have the styling for finished or not before possible to delete.
         todoItem.remove();
     });
 });
